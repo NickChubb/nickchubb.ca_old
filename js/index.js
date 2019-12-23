dfdb55bbadf16e3fc20faa22e911ad5ed89f6c6a
Nick Chubb <nchubb@sfu.ca>
2019-12-23 15:09:51 -0800

    init commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function hideAbout(){
+  if($('#about').is(':hidden')){
+    $('#about').slideDown('slow');
+    //$('#about').css("display", "flex");
+    $('#about-button').css("background-color", "lightgrey");
+    $('#about-button').css("box-shadow", "1px 1px 1px #ADADAD inset").css("padding-bottom", "2px").css("color", "green");
+  }else{
+    $('#about').slideUp('slow');
+    $('#about-button').css("background-color", "white");
+    $('#about-button').css("box-shadow", "0px 0px 0px #ADADAD inset").css("padding-bottom", "0px").css("color", "black");
+  }
+};
