bb8d8490c2821329db67d5c8c87ae0709364ad62
Nick Chubb <Nick.r.chubb@gmail.com>
2019-12-30 17:25:21 -0800

    updated bio and stuff

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ function hideAbout(){
   if($('#about').is(':hidden')){
     $('#about').slideDown('slow');
     //$('#about').css("display", "flex");
-    $('#about-button').css("background-color", "lightgrey");
-    $('#about-button').css("box-shadow", "1px 1px 1px #ADADAD inset").css("padding-bottom", "2px").css("color", "green");
+    $('#about-button').css("background-color", "#E8E8E8");
+    $('#about-button').css("box-shadow", "1px 1px 3px  #ADADAD inset, 0px 1px 3px #E8E8E8").css("padding-bottom", "2px").css("color", "green");
   }else{
     $('#about').slideUp('slow');
     $('#about-button').css("background-color", "white");
