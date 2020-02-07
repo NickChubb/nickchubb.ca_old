a2ba4dd037d415f1a57bf2f2c9102cd8a9ca0734
Nick Chubb <Nick.r.chubb@gmail.com>
2020-02-07 00:44:00 -0800

    updated css, background, shadows, other aesthetic changes

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@ function hideAbout(){
   if($('#about').is(':hidden')){
     $('#about').slideDown('slow');
     //$('#about').css("display", "flex");
-    $('#about-button').css("background-color", "#E8E8E8");
-    $('#about-button').css("box-shadow", "1px 1px 3px  #ADADAD inset, 0px 5px 0px #E8E8E8").css("padding-bottom", "2px").css("color", "green");
+    $('#about_button').css("background", "url('http://i.imgur.com/XwqVXjQ.png'), rgb(230,230,230)");
+    $('#about_button').css("box-shadow", "1px 1px 3px  #ADADAD inset").css("padding-bottom", "2px").css("color", "green").css("border-bottom", "5px solid rgb(230, 230, 230)").css("border-image-source", "url('http://i.imgur.com/XwqVXjQ.png')");
   }else{
     $('#about').slideUp('slow');
-    $('#about-button').css("background-color", "white");
-    $('#about-button').css("box-shadow", "0px 0px 0px #ADADAD inset").css("padding-bottom", "0px").css("color", "black");
+    $('#about_button').css("background", "white");
+    $('#about_button').css("box-shadow", "0px 0px 0px #ADADAD inset").css("padding-bottom", "0px").css("color", "black");
   }
 };
