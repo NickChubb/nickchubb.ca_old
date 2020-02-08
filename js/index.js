471e61b2e3c20755fd3eaeaa3b3662dfc98bae5a
Nick Chubb <Nick.r.chubb@gmail.com>
2020-02-07 16:17:41 -0800

    cleaned up js file so it's more readable

--- js/index.js
+++ js/index.js
@@ -1,12 +1,23 @@
 function hideAbout(){
   if($('#about').is(':hidden')){
+    //show the about section of the webstie
     $('#about').slideDown('slow');
-    //$('#about').css("display", "flex");
-    $('#about_button').css("background", "url('http://i.imgur.com/XwqVXjQ.png'), rgb(230,230,230)");
-    $('#about_button').css("box-shadow", "1px 1px 3px  #ADADAD inset").css("padding-bottom", "2px").css("color", "green").css("border-bottom", "5px solid rgb(230, 230, 230)").css("border-image-source", "url('http://i.imgur.com/XwqVXjQ.png')");
+
+    //Changing the CSS of the about_button
+    $('#about_button').css("background", "url(../images/grainy_background.png), rgb(230,230,230)");
+    $('#about_button').css("box-shadow", "1px 1px 3px  #ADADAD inset");
+    $('#about_button').css("padding-bottom", "2px");
+    $('#about_button').css("color", "green");
+    $('#about_button').css("border-bottom", "5px solid rgb(230, 230, 230)");
+    $('#about_button').css("border-image-source", "url(../images/grainy_background.png)");
   }else{
+    //hide the about section
     $('#about').slideUp('slow');
+
+    //reset the about_button to original css
     $('#about_button').css("background", "white");
-    $('#about_button').css("box-shadow", "0px 0px 0px #ADADAD inset").css("padding-bottom", "0px").css("color", "black");
+    $('#about_button').css("box-shadow", "0px 0px 0px #ADADAD inset");
+    $('#about_button').css("padding-bottom", "0px");
+    $('#about_button').css("color", "black");
   }
 };
